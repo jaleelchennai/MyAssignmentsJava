34616dfad207d96274bb79698ba87eab2863ec81
jaleelchennai <130042115+jaleelchennai@users.noreply.github.com>
2024-08-19 19:13:12 +0530

    Add files via upload

---
+++ one/Chrome.java
@@ -0,0 +1,19 @@
+package homeassignment.one;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float ver = 91.0F;
+		String browserName = "Google";
+		Boolean betaType = false;
+		int relYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("Version is: "+ver);
+		System.out.println("Browser name is: "+browserName);
+		System.out.println("Beta Type: "+betaType);
+		System.out.println("Released on: "+relYear);
+		System.out.println("Shortcut Key is: "+shortcutKey);
+
+	}
+
+}
